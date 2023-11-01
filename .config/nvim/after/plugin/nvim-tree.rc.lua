652c085b0bb71de044871bbe26dd2d8576b3338b
vqcuong <35632030+vqcuong@users.noreply.github.com>
2023-11-01 14:02:14 +0700

    Update nvim configuration

--- .config/nvim/after/plugin/nvim-tree.rc.lua
+++ .config/nvim/after/plugin/nvim-tree.rc.lua
@@ -6,19 +6,6 @@ if not status then
 	return
 end
 
-local status2, wp = pcall(require, "window-picker")
-if not status2 then
-	return
-end
-
-wp.setup({
-	show_prompt = false,
-	bo = {
-		filetype = { "NvimTree", "neo-tree", "notify", "packer", "Mason", "fugitive", "fugitiveblame" },
-		buftype = { "terminal", "nofile", "help" },
-	},
-})
-
 -- local function window_picker()
 -- 	local status, wp = pcall(require, "window-picker")
 -- 	if not status then
@@ -42,7 +29,7 @@ local function my_on_attach(bufnr)
 	end
 
 	-- default mappings
-	api.config.mappings.default_on_attach(bufnr)
+	-- api.config.mappings.default_on_attach(bufnr)
 
 	local function add_key(mode, new, func, desc)
 		vim.keymap.set(mode, new, func, opts(desc))
@@ -58,29 +45,59 @@ local function my_on_attach(bufnr)
 	end
 
 	-- custom mappings
-	replace_key("n", "<C-]>", "<Left>", api.tree.change_root_to_parent, "Up")
-	replace_key("n", "-", "<Right>", api.tree.change_root_to_node, "CD")
-	replace_key("n", "g?", "?", api.tree.toggle_help, "Help")
-	replace_key("n", "<C-v>", "vv", api.node.open.vertical, "Open File - split vertically")
-	replace_key("n", "<C-x>", "hh", api.node.open.horizontal, "Open File - split horizontally")
-	replace_key("n", "a", "nn", api.fs.create, "New")
-	replace_key("n", "c", "cc", api.fs.copy.node, "Copy")
-	replace_key("n", "gy", "cp", api.fs.copy.absolute_path, "Copy Absolute Path")
-	replace_key("n", "Y", "cr", api.fs.copy.relative_path, "Copy Relative Path")
-	replace_key("n", "y", "cn", api.fs.copy.filename, "Copy File Name")
-	replace_key("n", "r", "rr", api.fs.rename, "Rename")
-	replace_key("n", "e", "rb", api.fs.rename_basename, "Rename")
-	replace_key("n", "x", "xx", api.fs.cut, "Cut")
-	replace_key("n", "p", "pp", api.fs.paste, "Paste")
-	replace_key("n", "d", "dd", api.fs.remove, "Delete")
-	replace_key("n", "D", "dt", api.fs.trash, "Trash")
-	replace_key("n", "<C-k>", "ii", api.node.show_info_popup, "Get Info")
-	replace_key("n", "C", "gg", api.tree.toggle_git_clean_filter, "Git Clean Filter")
-	replace_key("n", "I", "gi", api.tree.toggle_gitignore_filter, "Git Ignore Filter")
-	del_key("n", "<C-r>")
-	del_key("n", "u")
-	-- add_key('n', 'zz', api.tree.focus, 'Focus')
-	-- add_key('n', 'tt', api.tree.toggle, 'Toggle')
+	add_key("n", "?", api.tree.toggle_help, "Help")
+	add_key("n", "<Left>", api.tree.change_root_to_parent, "Up")
+	add_key("n", "<Right>", api.tree.change_root_to_node, "CD")
+	add_key("n", "gc", api.tree.toggle_git_clean_filter, "Git Clean Filter")
+	add_key("n", "gi", api.tree.toggle_gitignore_filter, "Git Ignore Filter")
+	add_key("n", "E", api.tree.expand_all, "Expand All")
+	add_key("n", "W", api.tree.collapse_all, "Collapse All")
+	add_key("n", "q", api.tree.close, "Close Tree")
+	add_key("n", "R", api.tree.reload, "Refresh Tree")
+	add_key("n", "S", api.tree.search_node, "Search")
+	add_key("n", "D", api.tree.toggle_hidden_filter, "Filter Dotfiles")
+	add_key("n", "B", api.tree.toggle_no_buffer_filter, "Filter No Buffer")
+	add_key("n", "U", api.tree.toggle_custom_filter, "Filter Hidden")
+
+	add_key("n", "<CR>", api.node.open.edit, "Open")
+	add_key("n", "<Tab>", api.node.open.preview, "Preview")
+	add_key("n", "ii", api.node.show_info_popup, "Get Info")
+	add_key("n", "<C-e>", api.node.open.replace_tree_buffer, "Open In Place")
+	add_key("n", "<C-t>", api.node.open.tab, "Open In New Tab")
+	add_key("n", "vv", api.node.open.vertical, "Open - Split vertically")
+	add_key("n", "hh", api.node.open.horizontal, "Open - Split horizontally")
+	add_key("n", "<", api.node.navigate.sibling.prev, "Previous Sibling")
+	add_key("n", ">", api.node.navigate.sibling.next, "Next Sibling")
+	add_key("n", "[c", api.node.navigate.git.prev, "Previous Git")
+	add_key("n", "]c", api.node.navigate.git.next, "Next Git")
+	add_key("n", "[e", api.node.navigate.diagnostics.prev, "Previous Diagnostic")
+	add_key("n", "]e", api.node.navigate.diagnostics.next, "Next Diagnostic")
+	add_key("n", "K", api.node.navigate.sibling.first, "First Sibling")
+	add_key("n", "J", api.node.navigate.sibling.last, "Last Sibling")
+	add_key("n", "P", api.node.navigate.parent, "Parent Directory")
+	add_key("n", "<BS>", api.node.navigate.parent_close, "Close Directory")
+	add_key("n", "s", api.node.run.system, "Run System")
+	add_key("n", ".", api.node.run.cmd, "Run Command")
+
+	add_key("n", "nn", api.fs.create, "New")
+	add_key("n", "cc", api.fs.copy.node, "Copy")
+	add_key("n", "cp", api.fs.copy.absolute_path, "Copy Absolute Path")
+	add_key("n", "cr", api.fs.copy.relative_path, "Copy Relative Path")
+	add_key("n", "cn", api.fs.copy.filename, "Copy File Name")
+	add_key("n", "rr", api.fs.rename, "Rename")
+	add_key("n", "rb", api.fs.rename_basename, "Rename")
+	add_key("n", "xx", api.fs.cut, "Cut")
+	add_key("n", "pp", api.fs.paste, "Paste")
+	add_key("n", "dd", api.fs.remove, "Delete")
+	add_key("n", "dt", api.fs.trash, "Trash")
+
+	add_key("n", "md", api.marks.bulk.delete, "Delete Bookmarked")
+	add_key("n", "mt", api.marks.bulk.trash, "Trash Bookmarked")
+	add_key("n", "mv", api.marks.bulk.move, "Move Bookmarked")
+	add_key("n", "mm", api.marks.toggle, "Toggle Bookmark")
+
+	add_key("n", "F", api.live_filter.clear, "Clean Filter")
+	add_key("n", "f", api.live_filter.start, "Filter")
 end
 
 tree.setup({
@@ -91,24 +108,24 @@ tree.setup({
 				enable = true,
 				-- picker = window_picker,
 				chars = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890",
-        exclude = {
-          filetype = {
-            "notify",
-            "NvimTree",
-            "neo-tree",
-            "neo-tree-popup",
-            "packer",
-            "qf",
-            "diff",
-            "fugitive",
-            "fugitiveblame",
-          },
-          buftype = {
-            "nofile",
-            "terminal",
-            "help"
-          }
-        }
+				exclude = {
+					filetype = {
+						"notify",
+						"NvimTree",
+						"neo-tree",
+						"neo-tree-popup",
+						"packer",
+						"qf",
+						"diff",
+						"fugitive",
+						"fugitiveblame",
+					},
+					buftype = {
+						"nofile",
+						"terminal",
+						"help",
+					},
+				},
 			},
 		},
 	},
@@ -118,9 +135,8 @@ local function opts(desc)
 	return { desc = "NvimTree: " .. desc, noremap = true, silent = true, nowait = true }
 end
 
-vim.keymap.set("n", "G", '<CMD>lua require("nvim-tree.api").git.reload()<CR>', opts("Git Reload"))
-vim.keymap.set("n", "zz", "<CMD>NvimTreeFocus<CR>", opts("Focus"))
-vim.keymap.set("n", "tt", "<CMD>NvimTreeToggle<CR>", opts("Toggle"))
+vim.keymap.set("n", "G", "<CMD>lua require('nvim-tree.api').git.reload()<CR>", opts("Git Reload"))
+vim.keymap.set("n", "zz", "<CMD>NvimTreeFocus<CR>", opts("Focus Tree"))
+vim.keymap.set("n", "tt", "<CMD>NvimTreeToggle<CR>", opts("Toggle Tree"))
 vim.keymap.set("n", "bb", "<CMD>NvimTreeClipboard<CR>", opts("Show Clipboard"))
-vim.keymap.set("n", "bc", '<CMD>lua require("nvim-tree.api").fs.clear_clipboard()<CR>', opts("Clear Clipboard"))
-
+vim.keymap.set("n", "bc", "<CMD>lua require('nvim-tree.api').fs.clear_clipboard()<CR>", opts("Clear Clipboard"))
