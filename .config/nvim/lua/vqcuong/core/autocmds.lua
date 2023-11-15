91c945bef2607a0f4e78c3df9db2159fac0e05ab
vqcuong <35632030+vqcuong@users.noreply.github.com>
2023-11-15 23:29:25 +0700

    Update nvim configuration

--- .config/nvim/lua/vqcuong/core/autocmds.lua
+++ .config/nvim/lua/vqcuong/core/autocmds.lua
@@ -1,33 +1,32 @@
 -- disable auto comment when insert new line after comment
 vim.api.nvim_create_autocmd("BufEnter", {
-	group = vim.api.nvim_create_augroup("FormatOptions", {}),
-	pattern = "*",
-	callback = function()
-		vim.opt.formatoptions:remove({ "r", "o" })
-	end,
+  group = vim.api.nvim_create_augroup("FormatOptions", {}),
+  pattern = "*",
+  callback = function()
+    vim.opt.formatoptions:remove({ "r", "o" })
+  end,
 })
 
 -- instantly hide diagnostic when entering insert mode
 vim.api.nvim_create_autocmd("InsertEnter", {
-	group = vim.api.nvim_create_augroup("HideDiagnostic", {}),
-	pattern = "*",
-	callback = function()
-		vim.diagnostic.hide(nil, 0)
-	end,
+  group = vim.api.nvim_create_augroup("HideDiagnostic", {}),
+  pattern = "*",
+  callback = function()
+    vim.diagnostic.hide(nil, 0)
+  end,
 })
 
 -- instantly show diagnostic when leaving insert mode
 vim.api.nvim_create_autocmd("InsertLeave", {
-	group = vim.api.nvim_create_augroup("ShowDiagnostic", {}),
-	pattern = "*",
-	callback = function()
-		vim.diagnostic.show(nil, 0)
-	end,
+  group = vim.api.nvim_create_augroup("ShowDiagnostic", {}),
+  pattern = "*",
+  callback = function()
+    vim.diagnostic.show(nil, 0)
+  end,
 })
 
 -- disable paste when leaving insert mode
 vim.api.nvim_create_autocmd("InsertLeave", {
   pattern = "*",
-  command = "set nopaste"
+  command = "set nopaste",
 })
-
