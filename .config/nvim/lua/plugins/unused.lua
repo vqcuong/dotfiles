ae1c8f11f264558bac00e802f215887c0bdc2547
vqcuong <35632030+vqcuong@users.noreply.github.com>
2023-12-11 00:09:40 +0700

    Update nvim configuration

--- .config/nvim/lua/plugins/unused.lua
+++ .config/nvim/lua/plugins/unused.lua
@@ -35,4 +35,36 @@ return {
       { "<localleader>z", "<cmd>ZenMode<cr>", silent = true, desc = "Zen mode" },
     },
   },
+  {
+    "aserowy/tmux.nvim",
+    opts = {
+      copy_sync = {
+        enable = false,
+      },
+      navigation = {
+        -- cycles to opposite pane while navigating into the border
+        cycle_navigation = true,
+        -- enables default keybindings (C-hjkl) for normal mode
+        enable_default_keybindings = false,
+        -- prevents unzoom tmux when navigating beyond vim border
+        persist_zoom = false,
+      },
+      resize = {
+        -- enables default keybindings (A-hjkl) for normal mode
+        enable_default_keybindings = false,
+        resize_step_x = 1,
+        resize_step_y = 1,
+      },
+    },
+    keys = {
+      { "<m-left>", "<cmd>lua require('tmux').move_left()<cr>", desc = "Move left", mode = { "n", "t" } },
+      { "<m-right>", "<cmd>lua require('tmux').move_right()<cr>", desc = "Move right", mode = { "n", "t" } },
+      { "<m-up>", "<cmd>lua require('tmux').move_top()<cr>", desc = "Move up", mode = { "n", "t" } },
+      { "<m-down>", "<cmd>lua require('tmux').move_bottom()<cr>", desc = "Move down", mode = { "n", "t" } },
+      { "<m-h>", "<cmd>lua require('tmux').resize_left()<cr>", desc = "Resize left", mode = { "n", "t" } },
+      { "<m-l>", "<cmd>lua require('tmux').resize_right()<cr>", desc = "Resize right", mode = { "n", "t" } },
+      { "<m-j>", "<cmd>lua require('tmux').resize_top()<cr>", desc = "Resize up", mode = { "n", "t" } },
+      { "<m-k>", "<cmd>lua require('tmux').resize_bottom()<cr>", desc = "Resize down", mode = { "n", "t" } },
+    },
+  },
 }
