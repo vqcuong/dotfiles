41fe3776c53d1047ef56ac8ef114b7ccd101ce42
vqcuong <35632030+vqcuong@users.noreply.github.com>
2024-01-09 16:51:22 +0700

    Add fish configuration

---
+++ .config/fish/config.fish
@@ -0,0 +1,105 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+if test -f $HOME/.config/fish/config.local.fish
+    source $HOME/.config/fish/config.local.fish
+end
+if test -d $HOME/.config/fish/.local
+    for f in $HOME/.config/fish/.local/*
+        if test -f $f
+            source $f
+        end
+    end
+end
+if test -d $HOME/.config/fish/user-functions
+    for f in $HOME/.config/fish/user-functions/*
+        if test -f $f
+            source $f
+        end
+    end
+end
+
+# set -U fish_user_paths /opt/homebrew/bin/ $fish_user_paths
+/opt/homebrew/bin/brew shellenv | source
+
+# add fish completions from homebrew
+if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
+
+# user configuration
+thefuck --alias | source
+zoxide init fish | source
+
+# setup conda
+function _eval_conda
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    set conda_prefix $argv[1]
+    if test -f $conda_prefix/bin/conda
+        eval $conda_prefix/bin/conda "shell.fish" hook | source
+    end
+    # <<< conda initialize <<<
+end
+
+function _setup_conda
+    set conda_prefix $argv[1]
+    if test -z $conda_prefix
+        set conda_prefix $HOME/.apps/mambaforge
+    end
+    if test -d $conda_prefix
+        _eval_conda $conda_prefix
+    end
+    if ! command -v conda &>/dev/null
+        rm -rf $conda_prefix
+        set os_name $(uname -s)
+        set os_arch $(uname -m)
+        set tag_name $(http https://api.github.com/repos/conda-forge/miniforge/releases/latest | jq -r '.tag_name')
+        http -d https://github.com/conda-forge/miniforge/releases/download/$tag_name/Mambaforge-$os_name-$os_arch.sh -o /tmp/mambaforge.sh
+        bash /tmp/mambaforge.sh -b -p $conda_prefix
+        _eval_conda $conda_prefix
+    end
+end
+
+_setup_conda
+
+# setup pyenv
+function _setup_pyenv
+    set -g -x PYENV_ROOT $HOME/.apps/pyenv
+    fish_add_path -g -a $PYENV_ROOT/bin
+    if ! command -v pyenv &>/dev/null
+        mkdir -p $(dirname $PYENV_ROOT)
+        curl https://pyenv.run | bash
+        # git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT
+    end
+end
+_setup_pyenv
+
+# setup goenv
+function _setup_goenv
+    set -g -x GOENV_ROOT $HOME/.apps/goenv
+    set -g -x GOENV_GOPATH_PREFIX $HOME/.apps/go
+    set -g -x GOENV_GOMOD_VERSION_ENABLE 1
+    fish_add_path -g -a $GOENV_ROOT/bin
+    if ! command -v goenv &>/dev/null
+        mkdir -p $(dirname $GOENV_ROOT)
+        git clone https://github.com/go-nv/goenv.git $GOENV_ROOT
+    end
+    goenv init - | source
+    fish_add_path -g -a $GOENV_ROOT/shims
+end
+_setup_goenv
+
+if test "$TERM_PROGRAM" != WarpTerminal
+    starship init fish | source
+else
+    set POSH_THEMES_PATH $(brew --prefix oh-my-posh)/themes
+    set OMP_THEME catppuccin
+    oh-my-posh completion fish | source
+    oh-my-posh init fish --config $POSH_THEMES_PATH/$OMP_THEME.omp.json | source
+end
---
+++ .config/fish/user-functions/aliases.fish
@@ -0,0 +1,98 @@
+alias c='clear'
+alias f='fuck'
+alias omp='oh-my-posh'
+
+# ssh forward
+function sfw --description "sfw <host> <server_port> [<local_port>]"
+    set host $argv[1]
+    set server_port $argv[2]
+    set local_port $argv[3]
+    if test -z "$local_port"
+        set local_port $server_port
+    end
+    ssh -tL $local_port:localhost:$server_port $host
+end
+
+# exa
+alias ls='exa --icons --color=always --group --time-style=long-iso'
+alias l='ls -laH'
+alias la='ls -a'
+alias ll='ls -l'
+alias lla='ls -la'
+
+# docker-compose
+alias dc='docker-compose'
+alias dcu='docker-compose up -d'
+alias dcd='docker-compose down'
+
+# git
+alias g="git"
+alias gap="git apply"
+alias gcfg="git config --list"
+alias gcl="git clone"
+alias gf="git fetch"
+alias gfo="git fetch origin"
+alias gm="git merge"
+alias grv="git revert"
+alias grsh="git reset HEAD"
+alias grshh="git reset HEAD --hard"
+alias gst="git status"
+alias gsm="git submodule"
+alias gsmu="git submodule update"
+alias gsmur="git submodule update --recursive"
+# git commit
+alias gc="git commit -v"
+alias gcm="git commit -m"
+alias gca="git commit -v -a"
+alias gcam="git commit -a -m"
+# git diff
+alias gdf="git diff"
+alias 'gdf.'="git diff ."
+# git pull and push
+alias gp="git push"
+alias gpo="git push origin"
+alias gpl="git pull"
+alias gplo="git pull origin"
+# git remote
+alias grt="git remote -vv"
+alias grtv="git remote -v"
+alias grta="git remote add"
+alias grtmv="git remote rename"
+alias grtrm="git remote remove"
+alias grts="git remote set-url"
+alias grtu="git remote update"
+# git tag
+alias gts="git tag -s"
+# git local
+alias gad="git add"
+alias gaa="git add --all"
+alias gau="git add --update"
+alias gap="git add --patch"
+alias 'ga.'="git add ."
+alias grst="git restore"
+alias grss="git restore --source"
+alias grst="git restore --staged"
+# git worktree
+alias gwt="git worktree"
+
+# alias copy='function _copy(){ pbcopy < $1; }; _copy $@'
+alias copy='clipcopy'
+
+# trash files or folders instead of pernamently remove them
+alias rm='trash-put'
+
+# vim and nvim command
+alias v='vim'
+alias nv='nvim'
+alias n='nvim'
+alias 'n.'='nvim .'
+
+# lolcat wrapping
+set lolcat_cmds brew cat docker docker-compose du fd head kubectl more man pip rg tail
+set lolcat_args '$argv'
+for cmd in $lolcat_cmds
+    if command -v $cmd &>/dev/null
+        set func "function $cmd; command $cmd $lolcat_args | lolcat; end"
+        eval $func
+    end
+end
