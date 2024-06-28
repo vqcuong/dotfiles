bca159675c67530ebfccfbef82a41b022744c574
vqcuong96 <vqcuong96@gmail.com>
2024-06-29 01:32:59 +0700

    Update nvim

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,11 +1,7 @@
 return {
   {
     "williamboman/mason.nvim",
-    opts = function(_, opts)
-      table.insert(opts.ensure_installed, "isort")
-      table.insert(opts.ensure_installed, "autopep8")
-      table.insert(opts.ensure_installed, "beautysh")
-    end,
+    opts = { ensure_installed = { "isort", "autopep8", "beautysh" } },
   },
   {
     "nvimtools/none-ls.nvim",
@@ -14,11 +10,7 @@ return {
       local nls = require("null-ls")
       opts.sources = opts.sources or {}
       table.insert(opts.sources, nls.builtins.formatting.shellharden)
-      -- table.insert(opts.sources, nls.builtins.formatting.shfmt)
-
-      table.insert(opts.sources, nls.builtins.diagnostics.fish)
       table.insert(opts.sources, nls.builtins.diagnostics.zsh)
-
       table.insert(opts.sources, nls.builtins.hover.printenv)
     end,
   },
@@ -26,12 +18,9 @@ return {
     "stevearc/conform.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.formatters_by_ft["sh"] = { "shfmt" }
       opts.formatters_by_ft["bash"] = { "shfmt", "beautysh" }
       opts.formatters_by_ft["python"] = { "isort", "autopep8" }
       opts.formatters_by_ft["zsh"] = { "beautysh" }
-      opts.formatters_by_ft["fish"] = { "fish_indent" }
-      opts.formatters_by_ft["go"] = { "gofmt", "goimports" }
       opts.formatters_by_ft["json"] = { "prettier" }
       opts.formatters_by_ft["json"] = { "prettier" }
       opts.formatters_by_ft["html"] = { "prettier" }
@@ -39,7 +28,6 @@ return {
       opts.formatters_by_ft["js"] = { "prettier" }
       opts.formatters_by_ft["ts"] = { "prettier" }
       opts.formatters_by_ft["sql"] = { "sql_formatter" }
-      opts.formatters_by_ft["tf"] = { "terraform_fmt" }
 
       opts.formatters.autopep8 = {
         prepend_args = { "--indent-size", "2" },
