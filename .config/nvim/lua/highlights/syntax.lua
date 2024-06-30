1a06ca506fc86689ebf91d9a2ea1ecfc51226c59
vqcuong96 <vqcuong96@gmail.com>
2024-06-30 13:07:05 +0700

    Update zsh

--- .config/nvim/lua/highlights/syntax.lua
+++ .config/nvim/lua/highlights/syntax.lua
@@ -1,9 +1,11 @@
 local api = vim.api
 api.nvim_set_hl(0, "@variable.builtin", { fg = "#0dff00", bold = true })
+api.nvim_set_hl(0, "@variable.parameter", { fg = "#fa9daa", italic = true })
 api.nvim_set_hl(0, "@function.builtin", { fg = "#00b3ff", bold = true })
 api.nvim_set_hl(0, "@parameter", { fg = "#00fffb", italic = true })
 api.nvim_set_hl(0, "@property", { fg = "#ff80dd", italic = true })
 api.nvim_set_hl(0, "@field", { fg = "#ff80dd", italic = true })
+api.nvim_set_hl(0, "@keyword.import", { fg = "#cf3e6d", bold = true })
 -- api.nvim_set_hl(0, "@namespace", { fg = "#ff4089", italic = true })
 
 api.nvim_set_hl(0, "Search", { bg = "#445566" })
