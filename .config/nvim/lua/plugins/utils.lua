a0a5956d837caf0acecb08301b688be5b939f1f7
vqcuong96 <vqcuong96@gmail.com>
2024-07-19 22:07:12 +0700

    Update nvim

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -222,7 +222,36 @@ return {
 
   {
     "karb94/neoscroll.nvim",
-    config = true,
+    config = function()
+      local neoscroll = require("neoscroll")
+      local keymap = {
+        -- Use the "sine" easing function
+        ["<C-e>"] = function()
+          neoscroll.ctrl_u({ duration = 250, easing = "sine" })
+        end,
+        ["<C-d>"] = function()
+          neoscroll.ctrl_d({ duration = 250, easing = "sine" })
+        end,
+        -- Use the "circular" easing function
+        ["<C-c>"] = function()
+          neoscroll.ctrl_b({ duration = 450, easing = "circular" })
+        end,
+        ["<C-f>"] = function()
+          neoscroll.ctrl_f({ duration = 450, easing = "circular" })
+        end,
+        -- When no value is passed the `easing` option supplied in `setup()` is used
+        ["<C-y>"] = function()
+          neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 })
+        end,
+        ["<C-u>"] = function()
+          neoscroll.scroll(0.1, { move_cursor = false, duration = 100 })
+        end,
+      }
+      local modes = { "n", "v", "x" }
+      for key, func in pairs(keymap) do
+        vim.keymap.set(modes, key, func)
+      end
+    end,
   },
 
   {
@@ -248,7 +277,6 @@ return {
         function()
           require("ufo").closeAllFolds()
         end,
-        desc = "Close All Folds",
       },
     },
     config = function()
